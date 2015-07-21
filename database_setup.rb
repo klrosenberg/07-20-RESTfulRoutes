ab75b45bafd4546cd6e77ca479e23f23f2f56069
Kerry Rosenberg <rosenberg.kerry@gmail.com>
2015-07-21 11:32:35 -0500

    Fix database setup

--- database_setup.rb
+++ database_setup.rb
@@ -1,6 +1,3 @@
-# Create database for this program.
-DATABASE = SQLite3::Database.new("database.db")
-
 unless ActiveRecord::Base.connection.table_exists?(:users)
   ActiveRecord::Base.connection.create_table :users do |t|
     t.string :email
