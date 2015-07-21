54ba82d6f896ad659e3533f8117d68f7e972ab2c
Kasey McCormick <mccormick.kasey@gmail.com>
2015-07-21 14:30:10 -0500

    got edit/update to work on a story

--- controllers/stories_controller.rb
+++ controllers/stories_controller.rb
@@ -18,7 +18,22 @@ end
 # -----------------------------------------------------------------------------
 
 get "/stories/update" do
+
+  @story = Story.find(params["id"])
+  binding.pry
+  erb :"/stories/update_story_form"
 end
+
+put "/stories/update_save" do
+
+  @story = Story.find(params["id"])
+  @story.title = params["title"]
+  @story.save
+  
+  @allstories = Story.all
+  erb :"/stories/all_list"
+end
+
 
 # -----------------------------------------------------------------------------
 # Signed in user Creates a story CANNOT create a story for another user 
@@ -28,7 +43,7 @@ get '/stories/new' do
   erb :"/stories/new_story_form"
 end
 
-post "/stories/save" do
+post "/stories/update_save" do
   binding.pry
   user_id = params["user_id"]
   Story.create({"title" => params["title"], "user_id" => user_id})
@@ -44,78 +59,20 @@ end
 # -----------------------------------------------------------------------------
 
 get "/stories/select" do
-end
-
-
-
-# -----------------------------------------------------------------------------
-# Form to create new password.
-# -----------------------------------------------------------------------------
-get "/users/new" do
-  @user = User.new
-  erb :"/users/new"
+  erb :"/stories/complete_list"
 end
 
 # -----------------------------------------------------------------------------
-# Save new user and encrypt password.
+# deleting a story
 # -----------------------------------------------------------------------------
-post "/users/users/create" do
-  the_password = BCrypt::Password.create(params["password"])
-  
-  @user= User.create({name: params["name"], email: params["email"], password: the_password})
-  
-  if @user.valid?
-    # Make a GET request to the following path:
-    redirect "/users/#{@user.id}"
-  else
-    erb :"/users/new"
-  end
+
+get "/stories/delete" do
+  Story.delete(params["id"])
+  erb :"/stories/complete_list"
 end
 
-get "/users/edit" do
-  @user = User.find(params["id"])
-  erb :"/users/edit"
-end
 
-# -----------------------------------------------------------------------------
-# Show all users. Functionality to read, update or delete.
-# -----------------------------------------------------------------------------
-get "/users" do
-  @users = User.all
-  erb :"/users/users"
-end
 
-get "/users/select" do
-  @users = User.all
-  erb :"/users/users_delete_form"
-end
-
-get "/users/update" do
-  @users = User.all
-  erb :"/users/users_update_form"
-end
-
-delete "/users/delete" do
-  User.delete(params["id"].to_i)
-  @users = User.all
-  erb :"/users/users"
-end
-
-# -----------------------------------------------------------------------------
-# Show user profile after signing up.
-# -----------------------------------------------------------------------------
-get "/users/:id" do
-  @user = User.find(params[:id])
-  erb :"/users/profile"
-end
-
-put "/users/users/update" do
-  @user = User.find(params["id"])
-  @user.name = params["name"]
-  @user.email = params["email"]
-  @user.save
-  erb :"/users/profile"
-end
 
 
 
